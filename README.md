049566393070d5edcc1719ae2540609d29ea9b71
Lucas Santiago <lu.santi.oli@gmail.com>
2023-09-09 21:38:40 -0300

    feat: basic files

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # Ant-Colony-ROCm
+
 Ant Colony Opitmizations running in a ROCm enviroment
+
+# Running the application
+
+```
+chmod +x run.sh
+./run.sh
+```
---
+++ run.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+docker buildx build . -t "ant_colony_rocm"
+
+docker run -it --device=/dev/kfd --device=/dev/dri --group-add video "ant_colony_rocm"
