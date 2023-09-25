24039f3453ac6a2b870cbe1990ce2958a90434eb
Lucas Santiago <lu.santi.oli@gmail.com>
2023-09-25 07:42:47 -0300

    fix: now commands can fail without failing in cascade

--- run.sh
+++ run.sh
@@ -1,5 +1,3 @@
 #!/bin/bash
 
-docker buildx build . -t "ant_colony_rocm"
-
-docker run -it --device=/dev/kfd --device=/dev/dri --group-add video "ant_colony_rocm"
+docker buildx build . -t "ant_colony_rocm" && docker run -it --device=/dev/kfd --device=/dev/dri --group-add video "ant_colony_rocm"
