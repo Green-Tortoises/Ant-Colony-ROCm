8b4d231d54852f9acd0d077560580af9e2e15add
Lucas Santiago <lu.santi.oli@gmail.com>
2023-09-29 21:31:58 -0300

    feat: diving functions

--- src/acopar.cpp
+++ src/acopar.cpp
@@ -17,8 +17,21 @@ void AntColony::run() {
     // Getting the pointer to the matrix that has all the csv data
     Matrix *matrix_ptr = this->getMatrix();
 
+    int WIDTH = matrix_ptr->cols();
+    int HEIGHT = matrix_ptr->rows();
+
     // Getting default values
     this->createColony(matrix_ptr, d_matrix, d_colony_matrix);
+
+    // Getting data back from device
+    std::vector<float> d_result(WIDTH*HEIGHT);
+    HIP_CHECK(hipMemcpy(d_result.data(), d_matrix, WIDTH*HEIGHT*sizeof(float), hipMemcpyDeviceToHost));
+
+    std::vector<int> d_colony_result(HEIGHT*HEIGHT);
+    HIP_CHECK(hipMemcpy(d_colony_result.data(), d_colony_matrix, HEIGHT*HEIGHT*sizeof(int), hipMemcpyDeviceToHost));
+
+    matrix_ptr->data_.clear();
+    matrix_ptr->data_ = d_result;
 
     // Freeing the memory on the GPU
     hipFree(d_matrix);
@@ -56,11 +69,4 @@ void AntColony::createColony(Matrix *matrix_ptr, float *d_matrix, int *d_colony_
     dim3 grid((HEIGHT + block.x - 1) / block.x, (HEIGHT + block.y - 1) / block.y);
     hipLaunchKernelGGL(create_colony_matrix, grid, block, 0, 0, d_colony_matrix, HEIGHT, HEIGHT);
     hipDeviceSynchronize();
-
-    // Getting data back from device
-    std::vector<float> d_result(matrix_size);
-    HIP_CHECK(hipMemcpy(d_result.data(), d_matrix, matrix_size*sizeof(float), hipMemcpyDeviceToHost));
-
-    std::vector<int> d_colony_result(HEIGHT*HEIGHT);
-    HIP_CHECK(hipMemcpy(d_colony_result.data(), d_colony_matrix, HEIGHT*HEIGHT*sizeof(int), hipMemcpyDeviceToHost));
 }
